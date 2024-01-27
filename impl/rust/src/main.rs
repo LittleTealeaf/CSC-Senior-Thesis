8df1b8412208759de8e8be27c2d34ac0baedcd3d
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-01-27 13:33:12 -0500

    explore tracking differences

--- impl/rust/src/main.rs
+++ impl/rust/src/main.rs
@@ -1,3 +1,9 @@
+use std::time::SystemTime;
+
 fn main() {
+    let start = SystemTime::now();
     println!("Hello");
+    let elapsed = start.elapsed().unwrap();
+
+    println!("{}", elapsed.as_nanos());
 }
