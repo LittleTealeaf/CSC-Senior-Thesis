7aebeda9873f3bd8f4a28e190dd2aeda70482941
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-02-19 14:31:37 -0500

    tensorflow start

---
+++ impl/tensorflow/tf-main.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import numpy as np
+
+import time
+
+data = tf.abs(np.array([1,2,3]))
+
+start = time.perf_counter_ns()
+
+data = tf.abs(np.array([1,2,3]))
+
+end = time.perf_counter_ns()
+
+elapsed = end - start
+print(elapsed)
