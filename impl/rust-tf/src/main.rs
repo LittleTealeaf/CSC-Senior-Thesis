2ba2c4865b34d493886f4b8b5cb9d330103a9c65
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-02-26 14:57:09 -0500

    tensorflow is confusing

--- impl/rust-tf/src/main.rs
+++ impl/rust-tf/src/main.rs
@@ -1,4 +1,8 @@
-use tensorflow::Tensor;
+use tensorflow::{
+    ops::{self, mat_mul, relu, Placeholder},
+    train::Optimizer,
+    DataType, Graph, Scope, Tensor,
+};
 
 extern crate tensorflow;
 
@@ -14,6 +18,7 @@ fn main() {
         .collect();
 
     let network = load_network(include_str!("../../../data/network")).collect::<Vec<_>>();
+    let back_prop = back_propagation(network, &mut Scope::new_root_scope());
 }
 
 fn load_network(string: &str) -> impl Iterator<Item = (Tensor<f64>, Tensor<f64>)> + '_ {
@@ -36,7 +41,7 @@ fn load_network(string: &str) -> impl Iterator<Item = (Tensor<f64>, Tensor<f64>)
             .split(',')
             .map(|i| i.parse::<f64>().unwrap())
             .collect::<Vec<_>>();
-        let bias = Tensor::new(&[output_size])
+        let bias = Tensor::new(&[1, output_size])
             .with_values(&bias_values)
             .unwrap();
 
@@ -50,4 +55,30 @@ fn load_network(string: &str) -> impl Iterator<Item = (Tensor<f64>, Tensor<f64>)
 
         (bias, weights)
     })
+}
+
+fn back_propagation(
+    layers: impl IntoIterator<Item = (Tensor<f64>, Tensor<f64>)>,
+    scope: &mut Scope,
+) {
+    let input = Placeholder::new()
+        .dtype(DataType::Double)
+        .shape([1u64, 10u64])
+        .build(&mut scope.with_op_name("input"))
+        .unwrap();
+
+    let label = Placeholder::new()
+        .dtype(DataType::Double)
+        .shape([1, 1])
+        .build(&mut scope.with_op_name("label"))
+        .unwrap();
+
+    let feed_forward = layers.into_iter().fold(input, |prev, (bias, weight)| {
+        let mul = mat_mul(input, weight, scope).unwrap();
+        let add = tensorflow::ops::add(mul, bias, scope).unwrap();
+        let output = relu(add, scope).unwrap();
+        output
+    });
+
+    let label = ops::sub(feed_forward, label, scope).unwrap();
 }
