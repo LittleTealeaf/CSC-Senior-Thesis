cb207e6d21a3cf88a506074840ff25a86e812e2d
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-02-27 13:44:42 -0500

    damn...

--- impl/rust-tf/src/main.rs
+++ impl/rust-tf/src/main.rs
@@ -1,7 +1,8 @@
 use tensorflow::{
     ops::{self, mat_mul, relu, Placeholder},
-    train::Optimizer,
-    DataType, Graph, Scope, Tensor,
+    train::{GradientDescentOptimizer, MinimizeOptions, Optimizer},
+    DataType, Graph, Operation, Scope, Session, SessionOptions, SessionRunArgs, Status, Tensor,
+    Variable,
 };
 
 extern crate tensorflow;
@@ -17,8 +18,88 @@ fn main() {
         .map(|line| line.split(',').filter_map(|n| n.parse().ok()).collect())
         .collect();
 
-    let network = load_network(include_str!("../../../data/network")).collect::<Vec<_>>();
-    let back_prop = back_propagation(network, &mut Scope::new_root_scope());
+    let network = load_network(include_str!("../../../data/network"));
+
+    let mut scope = Scope::new_root_scope();
+
+    let input = Placeholder::new()
+        .dtype(DataType::Double)
+        .shape([1u64, (data[0].len() - 1) as u64])
+        .build(&mut scope.with_op_name("input"))
+        .unwrap();
+
+    let label = Placeholder::new()
+        .dtype(DataType::Double)
+        .shape([1, 1])
+        .build(&mut scope.with_op_name("label"))
+        .unwrap();
+
+    let mut layer_in = input.clone();
+    let mut variables = Vec::new();
+
+    for (index, (bias, weights)) in network.into_iter().enumerate() {
+        let mut scope = scope.new_sub_scope(&format!("layer_{index}"));
+        let w = Variable::builder()
+            .const_initial_tensor(&weights)
+            .build(&mut scope.with_op_name("w"))
+            .unwrap();
+
+        let b = Variable::builder()
+            .const_initial_tensor(&bias)
+            .build(&mut scope.with_op_name("b"))
+            .unwrap();
+
+        layer_in = relu(
+            ops::add(
+                b.output().clone(),
+                ops::mat_mul(layer_in, w.output().clone(), &mut scope).unwrap(),
+                &mut scope,
+            )
+            .unwrap(),
+            &mut scope,
+        )
+        .unwrap();
+
+        variables.extend([w, b]);
+    }
+
+    let error = ops::sub(layer_in.clone(), label.clone(), &mut scope).unwrap();
+    let error_squared = ops::mul(error.clone(), error, &mut scope).unwrap();
+    let optimizer = GradientDescentOptimizer::new(ops::constant(0.1f64, &mut scope).unwrap());
+
+    let (variables, optimizer) = optimizer
+        .minimize(
+            &mut scope,
+            error_squared.clone().into(),
+            MinimizeOptions::default().with_variables(&variables),
+        )
+        .unwrap();
+
+    let mut options = SessionOptions::default();
+    options.set_target(&optimizer.name().unwrap()).unwrap();
+
+    let session = Session::new(&options, &scope.graph()).unwrap();
+
+    for bootstrap in bootstraps {
+        let input_tensor = Tensor::new(&[bootstrap.len() as u64, data[0].len() as u64 - 1])
+            .with_values(
+                &(bootstrap
+                    .iter()
+                    .flat_map(|i| data[*i][1..].to_vec().clone())
+                    .collect::<Vec<_>>()),
+            )
+            .unwrap();
+
+        let label_tensor = Tensor::new(&[bootstrap.len() as u64, 1])
+            .with_values(&(bootstrap.iter().map(|i| data[*i][0]).collect::<Vec<_>>()))
+            .unwrap();
+
+        let mut run_args = SessionRunArgs::new();
+        run_args.add_feed(&input, 0, &input_tensor);
+        run_args.add_feed(&label, 0, &label_tensor);
+
+        session.run(&mut run_args).unwrap();
+    }
 }
 
 fn load_network(string: &str) -> impl Iterator<Item = (Tensor<f64>, Tensor<f64>)> + '_ {
@@ -49,36 +130,10 @@ fn load_network(string: &str) -> impl Iterator<Item = (Tensor<f64>, Tensor<f64>)
             .flat_map(|line| line.split(',').map(|i| i.parse::<f64>().unwrap()))
             .collect::<Vec<_>>();
 
-        let weights = Tensor::new(&[output_size, input_size])
+        let weights = Tensor::new(&[input_size, output_size])
             .with_values(&weights_values)
             .unwrap();
 
         (bias, weights)
     })
-}
-
-fn back_propagation(
-    layers: impl IntoIterator<Item = (Tensor<f64>, Tensor<f64>)>,
-    scope: &mut Scope,
-) {
-    let input = Placeholder::new()
-        .dtype(DataType::Double)
-        .shape([1u64, 10u64])
-        .build(&mut scope.with_op_name("input"))
-        .unwrap();
-
-    let label = Placeholder::new()
-        .dtype(DataType::Double)
-        .shape([1, 1])
-        .build(&mut scope.with_op_name("label"))
-        .unwrap();
-
-    let feed_forward = layers.into_iter().fold(input, |prev, (bias, weight)| {
-        let mul = mat_mul(input, weight, scope).unwrap();
-        let add = tensorflow::ops::add(mul, bias, scope).unwrap();
-        let output = relu(add, scope).unwrap();
-        output
-    });
-
-    let label = ops::sub(feed_forward, label, scope).unwrap();
 }
