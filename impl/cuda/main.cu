963aa9dafc0e9abd488eb5a059e9caadec5f7fb9
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-03-03 09:25:53 -0500

    rename cudaMatrixMultiply

--- impl/cuda/main.cu
+++ impl/cuda/main.cu
@@ -1,32 +1,27 @@
 #include "cuda_runtime.h"
 
 #include <chrono>
-#include <iostream>
 #include <cuda_device_runtime_api.h>
 #include <cuda_runtime_api.h>
 #include <driver_types.h>
+#include <iostream>
 
 using namespace std;
 
-__global__ void matrixMultiply(double* vec, double* mat, double* res) {
-
-}
-
+__global__ void cudaMatrixMultiply(double *vec, double *mat, double *res) {}
 
 int main() {
 
-  double vec[] = { 0.1,0.2,0.3 };
+  double vec[] = {0.1, 0.2, 0.3};
 
   double mat[5 * 3];
   for (int i = 0; i < 5 * 3; i++) {
     mat[i] = 0.1 * i;
   }
 
-
-
-  double* cudaVec = 0;
-  double* cudaMat = 0;
-  double* cudaRes = 0;
+  double *cudaVec = 0;
+  double *cudaMat = 0;
+  double *cudaRes = 0;
 
   cudaMalloc(&cudaVec, sizeof(vec));
   cudaMalloc(&cudaMat, sizeof(mat));
@@ -38,7 +33,6 @@ int main() {
   cudaFree(&cudaVec);
   cudaFree(&cudaMat);
   cudaFree(&cudaRes);
-
 
   return 0;
 }
