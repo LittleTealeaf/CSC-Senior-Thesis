5a43f6666d9c53c885bc01a76745da99957fce44
Thomas Kwashnak <thomaskwashnak@gmail.com>
2024-03-11 11:01:31 -0400

    Implemented feed forward

--- impl/cuda/main.cu
+++ impl/cuda/main.cu
@@ -8,109 +8,147 @@
 
 using namespace std;
 
-class CudaTensor
-{
-private:
-  int rows;
-  int cols;
-  double *cuda;
 
-public:
-  CudaTensor(int rows, int cols)
-  {
-    this->cols = cols;
-    this->rows = rows;
-    cudaMalloc(&this->cuda, this->cols * this->rows * sizeof(double));
-  }
 
-  void setValues(double *values)
-  {
-    cudaMemcpy(this->cuda, values, this->cols * this->rows * sizeof(double), cudaMemcpyHostToDevice);
-  }
+__global__ void cudaFeedForward(double* inputs, double* weights, double* bias, double* out, int observations, int input, int output) {
+  int COL = blockIdx.x * blockDim.x + threadIdx.x; // Ouput
+  int ROW = blockIdx.y * blockDim.y + threadIdx.y; //  Observation
 
-  ~CudaTensor()
-  {
-    cudaFree(this->cuda);
+  if (ROW < observations && COL < output) {
+    double value = 0.0;
+    for (int i = 0; i < input; i++) {
+      value += inputs[input * ROW + i] * weights[i * output + COL];
+    }
+    value += bias[COL];
+    out[ROW * output + COL] = max(value, 0.0);
   }
-};
+}
 
 /**
  * Multiplying a n*m and m*w matrix together to get a n*w matrix
  */
-__global__ void cudaMatrixMultiply(double *a, double *b, double *out, int n, int m, int w)
+__global__ void cudaMatrixMultiply(double* a, double* b, double* out, int n, int m, int w)
 {
 
   int ROW = blockIdx.y * blockDim.y + threadIdx.y;
   int COL = blockIdx.x * blockDim.x + threadIdx.x;
 
-  if (ROW < n && COL < w)
-  {
+  if (ROW < n && COL < w) {
     double tmp = 0.0;
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++) {
       tmp += a[m * ROW + i] * b[i * w + COL];
     }
     out[ROW * w + COL] = tmp;
   }
 }
 
-void matrixMultiplication(double *a, double *b, double *out, int n, int m, int w)
-{
+
+class CudaTensor {
+private:
+  int rows;
+  int cols;
+  double* cuda;
+
+public:
+  CudaTensor() : rows(0), cols(0), cuda(nullptr) {}
+
+  CudaTensor(int rows, int cols) : cols(cols), rows(rows) {
+    cudaMalloc(&this->cuda, this->cols * this->rows * sizeof(double));
+  }
+
+  double* getCuda() {
+    return this->cuda;
+  }
+
+  void setValues(double* values) {
+    cudaMemcpy(this->cuda, values, this->cols * this->rows * sizeof(double), cudaMemcpyHostToDevice);
+  }
+
+  void getValues(double* values) {
+    cudaMemcpy(values, this->cuda, this->cols * this->rows * sizeof(double), cudaMemcpyDeviceToHost);
+  }
+
+  ~CudaTensor() {
+    cudaFree(this->cuda);
+  }
+};
+
+
+
+class NetworkLayer {
+private:
+  int input;
+  int output;
+  CudaTensor weights;
+  CudaTensor bias;
+public:
+  NetworkLayer(int input, int output) : input(input), output(output), weights(input, output), bias(output, 1) {}
+
+
+  void setWeights(double* values) {
+    this->weights.setValues(values);
+  }
+
+  void setBias(double* bias) {
+    this->bias.setValues(bias);
+  }
+
+  void feedForward(double* cuda_in, int observations, double* cuda_out) {
+    dim3 threadsPerBlock(this->output, observations);
+    dim3 blocksPerGrid(1, 1);
+
+    if (observations * this->output > 512) {
+      threadsPerBlock.x = 512;
+      threadsPerBlock.y = 512;
+      blocksPerGrid.x = ceil(double(this->output) / double(threadsPerBlock.x));
+      blocksPerGrid.y = ceil(double(observations) / double(threadsPerBlock.y));
+    }
+
+    cudaFeedForward << <blocksPerGrid, threadsPerBlock >> > (cuda_in, this->weights.getCuda(), this->bias.getCuda(), cuda_out, observations, this->input, this->output);
+  }
+};
+
+
+
+void matrixMultiplication(double* a, double* b, double* out, int n, int m, int w) {
   dim3 threadsPerBlock(w, n);
   dim3 blocksPerGrid(1, 1);
 
-  if (n * w > 512)
-  {
+  if (n * w > 512) {
     threadsPerBlock.x = 512;
     threadsPerBlock.y = 512;
     blocksPerGrid.x = ceil(double(w) / double(threadsPerBlock.x));
     blocksPerGrid.y = ceil(double(n) / double(threadsPerBlock.y));
   }
 
-  cudaMatrixMultiply<<<blocksPerGrid, threadsPerBlock>>>(a, b, out, n, m, w);
+  cudaMatrixMultiply << <blocksPerGrid, threadsPerBlock >> > (a, b, out, n, m, w);
 }
 
-int main()
-{
+int main() {
+  NetworkLayer layer = NetworkLayer(2, 2);
+  double weights[] = { 1.0,2.0,3.0,4.0 };
+  double bias[] = { 2.0,1.0 };
+  layer.setWeights(weights);
+  layer.setBias(bias);
 
-  // Multiplying a 3x3 matrix by 3x3
+  double input[] = { 1.0,1.0,2.0,2.0 };
 
-  int n = 700;
-  int m = 550;
-  int w = 700;
+  double* cuda_input;
+  cudaMalloc(&cuda_input, sizeof(input));
+  cudaMemcpy(cuda_input, input, sizeof(input), cudaMemcpyHostToDevice);
 
-  double mat_a[n * m];
-  double mat_b[m * w];
-  double mat_out[n * w];
+  double* cuda_output;
+  cudaMalloc(&cuda_output, sizeof(double) * 4);
 
-  for (int i = 0; i < n * m; i++)
-  {
-    mat_a[i] = sin(i);
-  }
+  layer.feedForward(cuda_input, 2, cuda_output);
 
-  for (int i = 0; i < m * w; i++)
-  {
-    mat_b[i] = cos(i);
-  }
 
-  double *cudaA;
-  double *cudaB;
-  double *cudaOut;
+  double output[4];
+  cudaMemcpy(output, cuda_output, sizeof(output), cudaMemcpyDeviceToHost);
 
-  cudaMalloc(&cudaA, sizeof(mat_a));
-  cudaMalloc(&cudaB, sizeof(mat_b));
-  cudaMalloc(&cudaOut, sizeof(double) * n * w);
 
-  cudaMemcpy(cudaA, mat_a, sizeof(mat_a), cudaMemcpyHostToDevice);
-  cudaMemcpy(cudaB, mat_b, sizeof(mat_b), cudaMemcpyHostToDevice);
+  cout << output[0] << " " << output[1] << " " << output[2] << " " << output[3] << endl;
 
-  matrixMultiplication(cudaA, cudaB, cudaOut, n, m, w);
-
-  cudaMemcpy(mat_out, cudaOut, sizeof(mat_out), cudaMemcpyDeviceToHost);
-
-  cudaFree(cudaA);
-  cudaFree(cudaB);
-  cudaFree(cudaOut);
-
-  return 0;
+  cudaFree(cuda_input);
+  cudaFree(cuda_output);
 }
